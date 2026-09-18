bd36cba057f97174a6bf535ebc95938f903226ec
Alexandrawinsnes <alexandrawinsnes@localhost.localdomain>
2026-09-18 14:52:06 +0200

    a relevant commit message

---
+++ a0_git_github/README.m
@@ -0,0 +1,13 @@
+# Git and Github
+
+- git is a version control system locally 
+- github is a cloud platform for storing git repositorys also known as ´repos´
+
+## commit and push
+
+´´´bash
+git add .
+
+
+
+
---
+++ a0_git_github/README.md
@@ -0,0 +1,11 @@
+# Git and Github
+
+- git is a version control system locally 
+- github is a cloud platform for storing git repositorys also known as ´repos´
+
+## commit and push
+
+´´´bash
+git add .
+
+
